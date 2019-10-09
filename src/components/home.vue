ad2607271074c7cbf2d9cd856ae30d34d11a3184
iksen <38707145@qq.com>
2019-10-09 11:38:39 +0800

    添加友链

--- src/components/home.vue
+++ src/components/home.vue
@@ -305,9 +305,17 @@
 					 <h4><strong>友情链接</strong></h4>
 				 </div>
 				 <div class="box-body">
-					<table width="100%" border="0" cellpadding="3" cellspacing="0">
+					<!-- <table width="100%" border="0" cellpadding="3" cellspacing="0">
 						<tbody>
 							<tr>
+								<td><el-link href="http://mail.qq.com/cgi-bin/qm_share?t=qm_mailme&email=d05FTkBPTk5GRjcGBlkUGBo" target="_blank" type="info">申请友链</el-link></td>
+							</tr>
+						</tbody>
+					</table> -->
+					<table border="0" cellpadding="3" cellspacing="0">
+						<tbody>
+							<tr>
+								<td style=" padding-right: 20px;"><el-link href="https://daohang.aizhancloud.cn/76" target="_blank" type="info">免费收录网站</el-link></td>
 								<td><el-link href="http://mail.qq.com/cgi-bin/qm_share?t=qm_mailme&email=d05FTkBPTk5GRjcGBlkUGBo" target="_blank" type="info">申请友链</el-link></td>
 							</tr>
 						</tbody>
