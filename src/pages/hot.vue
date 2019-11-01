889af6fc0a9e7b889d067ed98cfeb77ccda1b29a
iksen <38707145@qq.com>
2019-11-01 16:17:20 +0800

    m

--- src/pages/hot.vue
+++ src/pages/hot.vue
@@ -5,7 +5,7 @@
 			  <ul class="aside">
 				  <li><a href="javascript:void(0)" class="active" @click="tabs('tab1',$event),baiduClick({index:0})">百度风云榜单</a></li>
 				  <li><a href="javascript:void(0)"  @click="tabs('tab2',$event),weiboClick({index:0})">微博热搜榜</a></li>
-				  <li><a href="javascript:void(0)"  @click="tabs('tab2',$event),weiboClick({index:0})">知乎热搜榜</a></li>
+				  <li><a href="javascript:void(0)"  @click="tabs('tab3',$event),weiboClick({index:0})">知乎热搜榜</a></li>
 				  <li><a href="javascript:void(0)"  @click="tabs('tab2',$event),weiboClick({index:0})">豆瓣排行榜</a></li>
 			  </ul>
 		  </el-aside>
@@ -17,19 +17,15 @@
 								<span>刚刚更新-数据源自百度</span>
 							</div>
 							<div class="scrollbar" style="border-left: 1px #F0F0F0 solid;border-right: 1px #F0F0F0 solid;border-bottom: 1px #F0F0F0 solid;">
-							  <table>
-								 <tr v-for="realTime of baidu.realTime">
-									<td><a :href="realTime.url" target="_blank" class="item" :title="'百度实时热点-'+realTime.title">
-											<span class="number tag">{{realTime.id}}</span> {{realTime.title}}
-									</a></td>
-									<td width="15%">
-										<span class="level">{{realTime.level}}</span>
-									</td>
-									<td width="8%" style="text-align: center;">
-										<span class="trend"><i :class="realTime.trend == 'rise' ? 'rise el-icon-top':'fall el-icon-bottom'"></i></span>
-									</td>
-								 </tr>
-							  </table>
+							  <ul>
+								  <li v-for="realTime of baidu.realTime">
+									  <a :href="realTime.url" target="_blank" class="item" :title="'百度实时热点-'+realTime.title">
+									  		<div class="title"><span class="number tag">{{realTime.id}}</span> {{realTime.title}}</div>
+											<span class="level">{{realTime.level}}</span>
+											<span class="trend"><i :class="realTime.trend == 'rise' ? 'rise el-icon-top':'fall el-icon-bottom'"></i></span>
+									  </a>
+								  </li>
+							  </ul>
 							</div>
 						</el-tab-pane>
 						<el-tab-pane label="百度今日热点榜" name="baidu_tab2">
@@ -37,19 +33,15 @@
 								<span>刚刚更新-数据源自百度</span>
 							</div>
 							<div class="scrollbar" style="border-left: 1px #F0F0F0 solid;border-right: 1px #F0F0F0 solid;border-bottom: 1px #F0F0F0 solid;">
-							  <table>
-								 <tr v-for="today of baidu.today">
-									<td><a :href="today.url" target="_blank" class="item" :title="'百度今日热点-'+today.title">
-											<span class="number tag">{{today.id}}</span> {{today.title}}
-									</a></td>
-									<td width="15%">
-										<span class="level">{{today.level}}</span>
-									</td>
-									<td width="8%" style="text-align: center;">
-										<span class="trend"><i :class="today.trend == 'rise' ? 'rise el-icon-top':'fall el-icon-bottom'"></i></span>
-									</td>
-								 </tr>
-							  </table>
+							  <ul>
+								  <li v-for="today of baidu.today">
+									  <a :href="today.url" target="_blank" class="item" :title="'百度今日热点-'+today.title">
+											<div class="title"><span class="number tag">{{today.id}}</span> {{today.title}}</div>
+											<span class="level">{{today.level}}</span>
+											<span class="trend"><i :class="today.trend == 'rise' ? 'rise el-icon-top':'fall el-icon-bottom'"></i></span>
+									  </a>
+								  </li>
+							  </ul>
 							</div>
 						</el-tab-pane>
 						<el-tab-pane label="百度百科热词" name="baidu_tab3">
@@ -95,20 +87,18 @@
 		  					<span>刚刚更新-数据源自微博</span>
 		  				</div>
 		  				<div class="scrollbar" style="border-left: 1px #F0F0F0 solid;border-right: 1px #F0F0F0 solid;border-bottom: 1px #F0F0F0 solid;">
-		  				  <table>
-		  					 <tr v-for="realTime of weibo.realTime">
-		  						<td><a :href="realTime.url" target="_blank" class="item" :title="'微博实时热点-'+realTime.title">
-		  							<span class="number tag" v-if="realTime.id == 0"><i class="el-icon-download" style="transform: rotate(180deg);"></i></span>
-									<span class="number tag" v-else>{{realTime.id}}</span> {{realTime.title}}
-		  						</a></td>
-		  						<td width="15%">
-		  							<span class="level">{{realTime.level}}</span>
-		  						</td>
-		  						<td width="8%" style="text-align: center;">
-		  							<span class="trend tag" v-if="realTime.trend != ''">{{realTime.trend}}</span>
-		  						</td>
-		  					 </tr>
-		  				  </table>
+		  				  <ul>
+							  <li v-for="realTime of weibo.realTime">
+								  <a :href="realTime.url" target="_blank" class="item" :title="'微博实时热点-'+realTime.title">
+										<div class="title">
+											<span class="number tag" v-if="realTime.id == 0"><i class="el-icon-download" style="transform: rotate(180deg);"></i></span>
+											<span class="number tag" v-else>{{realTime.id}}</span> {{realTime.title}}
+										</div>
+										<span class="level">{{realTime.level}}</span>
+										<span><span class="trend tag" v-if="realTime.trend != ''">{{realTime.trend}}</span></span>
+								  </a>
+							  </li>
+		  				  </ul>
 		  				</div>
 		  			</el-tab-pane>
 					<el-tab-pane label="微博新时代榜" name="weibo_tab2">
@@ -116,17 +106,56 @@
 							<span>刚刚更新-数据源自微博</span>
 						</div>
 						<div class="scrollbar" style="border-left: 1px #F0F0F0 solid;border-right: 1px #F0F0F0 solid;border-bottom: 1px #F0F0F0 solid;">
-						  <table>
-							 <tr v-for="socialEvent of weibo.socialEvent">
-								<td><a :href="socialEvent.url" target="_blank" class="item" :title="'新时代-'+socialEvent.title">
-									<i class="el-icon-paperclip"></i> {{socialEvent.title}}
-								</a></td>
-							 </tr>
-						  </table>
+						  <ul>
+							 <li v-for="socialEvent of weibo.socialEvent">
+								<a :href="socialEvent.url" target="_blank" class="item" :title="'新时代-'+socialEvent.title">
+									<span><i class="el-icon-paperclip"></i> {{socialEvent.title}}</span>
+								</a>
+							 </li>
+						  </ul>
 						</div>
 					</el-tab-pane>
 				</el-tabs>
 		      </div>
+			  
+			  <div class="tabpanel" name='tab3'>
+				<el-tabs v-model="info.zhihu.active" type="card" @tab-click="zhihuClick">
+					<el-tab-pane label="知乎热搜" name="zhihu_tab1">
+						<div class="hot-header">
+							<span>刚刚更新-数据源自知乎</span>
+						</div>
+						<div class="scrollbar" style="border-left: 1px #F0F0F0 solid;border-right: 1px #F0F0F0 solid;border-bottom: 1px #F0F0F0 solid;">
+						  <ul>
+							 <li v-for="(topSearch,index) of zhihu.topSearch">
+								<a :href="topSearch.url" target="_blank" class="item" :title="'知乎-'+topSearch.title">
+									<span style="width: 60%;"><i class="el-icon-paperclip"></i> {{topSearch.title}}</span>
+									<span>{{topSearch.summary}}</span>
+								</a>
+							 </li>
+						  </ul>
+						</div>
+					</el-tab-pane>
+					<el-tab-pane label="知乎热榜" name="zhihu_tab2">
+						<div class="hot-header">
+							<span>刚刚更新-数据源自知乎</span>
+						</div>
+						<div class="scrollbar" style="border-left: 1px #F0F0F0 solid;border-right: 1px #F0F0F0 solid;border-bottom: 1px #F0F0F0 solid;">
+						  <ul class="article">
+							 <li v-for="(topHot,index) of zhihu.topHot">
+								<a class="item" :href="topHot.url" target="_blank" title="topHot.title">
+									<div class="article-index"><span class="number">{{index+1}}</span></div>
+									<div class="article-content">
+										<h3 class="h3">{{topHot.title}}</h3>
+										<p class="article-excerpt">{{topHot.summary}}</p>
+									</div>
+									<div class="article-img"><img :src="topHot.img" v-if="topHot.img" :title="topHot.title" :alt="topHot.title"/></div>
+								</a>
+							 </li>
+						  </ul>
+						</div>
+					</el-tab-pane>
+				</el-tabs>
+			  </div>
 		  </el-main>
 		</el-container>
 	</main-layout>
@@ -146,6 +175,10 @@ default {
 					weibo:{
 						url : "/hom1/api/hot/weibo?t=",
 						active : "weibo_tab1"
+					},
+					zhihu:{
+						url : "/hom1/api/hot/zhihu?t=",
+						active : "zhihu_tab1"
 					}
 				},
 				baidu:{
@@ -156,6 +189,10 @@ default {
 				weibo:{
 					realTime:[],
 					socialEvent:[]
+				},
+				zhihu:{
+					topSearch:[],
+					topHot:[]
 				}
 			}
 		},
@@ -205,6 +242,27 @@ default {
 					else if(tab.index == 1)
 						this.weibo.socialEvent = response.data.result
 
+				}).catch((response)=>{
+				  loading.close();
+				  this.$message.error('数据请求失败，请稍后再试');
+				});
+			},
+			zhihuClick:function(tab,event){
+				const loading = this.$loading({
+				  lock: true,
+				  text: 'Loading',
+				  spinner: 'el-icon-loading',
+				  background: 'rgba(0, 0, 0, 0.2)'
+				});
+				this.$ajax.get('/hom1/api/hot/zhihu?t=' + tab.index)
+				.then((response)=>{
+					loading.close();
+					
+					if(tab.index == 0)
+						this.zhihu.topSearch = response.data.result
+					else if(tab.index == 1)
+						this.zhihu.topHot = response.data.result
+			
 				}).catch((response)=>{
 				  loading.close();
 				  this.$message.error('数据请求失败，请稍后再试');
@@ -291,22 +349,73 @@ default {
 		overflow-y:auto;
 		
 	}
-	.content table{
-		width: 100%;
-		border-collapse: collapse;
-	}
 	
-	.content tr{
+	.content li{
 		height: 50px;
+		line-height: 50px;
+		color: #909399;
 		cursor: pointer;
 		border-bottom: 1px #E8E8E8 dotted;
+	}
+	
+	.content .article li{
+		height: auto;
+		line-height: inherit;
+		padding: 15px 10px;
+	}
+	
+	.content .article .article-img{
+		display: block;
+		height: 105px;
+		margin-left: 16px;
+		position: relative;
+	}
+	
+	.content .article .article-img img{
+		border-radius: 4px;
+		height: 105px;
+		width: 190px;
+		display: block;
+		object-fit: cover;
+	}
+	
+	.content .article .article-content{
+		flex: 1 1;
+		overflow: hidden;
+	}
+	
+	.content .article .article-index{
+		width: 35px;
+	}
+	
+	.content .article .number{
+		font-size: 18px;
+		font-weight: 600;
+		line-height: 1.6;
+		color: #909399;
+	}
+	
+	.content .article .article-excerpt{
+		color: #444;
+		line-height: 25px;
+		margin-top: 2px;
+		overflow: hidden;
+		text-overflow: ellipsis;
+		white-space: nowrap;
+	}
+	
+	.article li:nth-child(1) .number, .article li:nth-child(2) .number, .article li:nth-child(3) .number{
+		color: #ff9607;
 	}
 	
 	.content .item{
 		font-size: 16px;
 		padding-left: 22px;
+		display: flex;
 	}
-	
+	.content .item .title{
+		width: 78%;
+	}
 	.content .item .number{
 		padding: 2px 6px;
 	}
@@ -320,6 +429,7 @@ default {
 	}
 	
 	.level{
+		width: 17%;
 		font-size: 13px;
 		color: #666666;
 	}
@@ -336,18 +446,34 @@ default {
 		padding: 2px;
 	}
 	
-	.content tr:nth-child(1) .tag{
+	.content li:nth-child(1) .tag{
 		background-color: #FF5722;
 	}
 	
-	.content tr:nth-child(2) .tag{
+	.content li:nth-child(2) .tag{
 		background-color: #FFB800;
 	}
 	
-	.content tr:hover a{
+	.content li:hover a{
 		color: #5FB878;
 	}
 	
+	.content img{
+		height: 90px;
+		width: 140px;
+	}
+	
+	.content .article h3{
+		color: #1a1a1a;
+		font-size: 17px;
+		line-height: 28px;
+	}
+	
+	.content .article p{
+		color: #444;
+		font-size: 14px;
+		
+	}
 	
 	.hot-header{
 		width: 100%;
