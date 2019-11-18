4d854138719cfa3a2f31efa9241b9af4da050e44
lycnihao <38707145@qq.com>
2019-11-18 21:18:46 +0800

    m

--- src/components/tianqi_plugin.vue
+++ src/components/tianqi_plugin.vue
@@ -1,31 +1,20 @@
 <template>
-	<div id="he-plugin-standard"></div>
+	<div id="tianqi">
+		
+	</div>
 </template>
 
-<script>
-	
-</script>
 <script>
 					
 	export
 	default {
 		created(){
-			(function (d) {
-			  var c = d.createElement('link')
-			  c.rel = 'stylesheet'
-			  c.href = 'https://widget.heweather.net/standard/static/css/he-standard.css?v=1.1'
-			  
-			  var s = d.createElement('script')
-			  s.src = 'https://widget.heweather.net/standard/static/js/he-standard.js?v=1.1'
-			  
-			  
-			  var w = d.createElement('script')		
-			  w.innerHTML = 'WIDGET = {CONFIG: {"layout": 1,"width": 450,"height": 150,"background": 1,"dataColor": "FFFFFF", "borderRadius": 5,"key": "05bcfc7d9024455fa19eed3553636c14"}}';
-			  
-			  var sn = d.getElementsByTagName('script')[0]
-			  sn.parentNode.insertBefore(c, sn)
-			  sn.parentNode.insertBefore(s, sn)
-			})(document)
+			window.addEventListener('scroll', function(){
+				this.$ajax.get('https://free-api.heweather.net/s6/weather/now?location=changsha&key=f30496591f7c416781f3a9d5f84adf90')
+				.then((response)=>{
+				   console.log(response.data)
+				});
+			}, true);
 		}
 	}
 </script>
