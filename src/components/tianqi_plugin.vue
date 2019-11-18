fb99f1a6000cf5e4518b8b76c5687ba877dd4c97
lycnihao <38707145@qq.com>
2019-11-18 21:53:14 +0800

    m

--- src/components/tianqi_plugin.vue
+++ src/components/tianqi_plugin.vue
@@ -9,12 +9,11 @@
 	export
 	default {
 		created(){
-			window.addEventListener('scroll', function(){
-				this.$ajax.get('https://free-api.heweather.net/s6/weather/now?location=changsha&key=f30496591f7c416781f3a9d5f84adf90')
-				.then((response)=>{
-				   console.log(response.data)
-				});
-			}, true);
+			let city = 'changsha'
+			this.$ajax.get('/hom1/api/getWeather/?city=' + city)
+			.then((response)=>{
+			   console.log(response.data)
+			});
 		}
 	}
 </script>
